0c16bb09dd70d2e5427b4223b7a15135a00c8849
eduardoosher <15161394@itoaxaca.edu.mx>
2019-02-16 23:31:07 -0600

    Primer commit :)

--- resources/views/pw/insertar_datos.blade.php
+++ resources/views/pw/insertar_datos.blade.php
@@ -6,34 +6,49 @@
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body>
-    {!!Form::open(array('url'=>'insertar','method'=>'POST','autocomplete'=>'off','class'=>'form-vertical'))!!}
-    {!!Form::label('Alumno: ')!!}
-    <br>
-    {!!form::text('alumno',null)!!}
-    <br>
-    {!!Form::label('Appaterno: ')!!}
-    <br>
-    {!!form::text('appaterno',null)!!}
-    <br>
-    {!!Form::label('Apmaterno: ')!!}
-    <br>
-    {!!form::text('apmaterno',null)!!}
-    <br>
-    {!!Form::label('edad: ')!!}
-    <br>
-    {!!form::number('edad',null)!!}
-    <br>
-    {!!Form::label('direccion: ')!!}
-    <br>
-    {!!form::text('direccion',null)!!}
-    <br>
-    {!!Form::label('telefono: ')!!}
-    <br>
-    {!!form::number('telefono',null)!!}
 
-    {!!form::submit('Registrar',['name'=>'grabar','id'=>'grabar','content'=>'<span>registrar</span>','class'=>'btn btn-lg btn-info'])!!}
+   <div class="formulario container">
+     {!!Form::open(array('url'=>'insertar','method'=>'POST','autocomplete'=>'off'))!!}
+
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Alumno</span>
+       </div>
+      {!!form::text('alumno',null,['placeholder'=>'alumno'])!!}
+     </div>
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Ap paterno</span>
+       </div>
+      {!!form::text('appaterno',null,['placeholder'=>'apellido paterno'])!!}
+     </div>
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Ap materno</span>
+       </div>
+      {!!form::text('apmaterno',null,['placeholder'=>'apellido materno'])!!}
+     </div>
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Edad</span>
+       </div>
+      {!!form::number('edad',null,['placeholder'=>'18'])!!}
+     </div>
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Direccion</span>
+       </div>
+      {!!form::text('direccion',null,['placeholder'=>'Direccion'])!!}
+     </div>
+     <div class="input-group input-group-sm mb-3">
+       <div class="input-group-prepend">
+         <span class="input-group-text" id="basic-addon1">Telefono</span>
+       </div>
+      {!!form::number('Telefono',null,['placeholder'=>'000-000-00-00'])!!}
+     </div>
+      {!!form::submit('Registrar',['name'=>'grabar','id'=>'grabar','content'=>'<span>registrar</span>','class'=>'btn btn-primary btn-info'])!!}
 
     {!!Form::close()!!}
-
+   </div>
   </body>
 </html>
